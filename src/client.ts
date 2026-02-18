df759d5142916353b97d20b290774aebab32598a
Jon Lambert <jonlambert@icloud.com>
2026-02-18 14:26:06 +0000

    fix: update to latest api version

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,8 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError } from 'axios';
 import {
   array,
   literal,
+  number,
   object,
   optional,
   string,
@@ -9,13 +10,13 @@ import {
   unknown,
   Output,
   safeParse,
-} from "valibot";
+} from 'valibot';
 import {
   LaunchDarklyAuthorizationError,
   CommandMissingAuthKeyError,
   LaunchDarklyUnexpectedResponse,
-} from "./errors";
-import { LAUNCHDARKLY_BASE_URL } from "./constants";
+} from './errors';
+import { LAUNCHDARKLY_BASE_URL, LAUNCHDARKLY_API_VERSION } from './constants';
 
 function getApiKeyFromEnv() {
   if (!process.env.LAUNCHDARKLY_API_KEY) {
@@ -31,13 +32,13 @@ function getApiKey(apiKey: string | undefined) {
 
 const flagSchema = union([
   object({
-    kind: literal("boolean"),
+    kind: literal('boolean'),
     key: string(),
     description: optional(string()),
     name: string(),
   }),
   object({
-    kind: literal("multivariate"),
+    kind: literal('multivariate'),
     key: string(),
     description: optional(string()),
     name: string(),
@@ -49,29 +50,43 @@ export type LaunchDarklyFlag = Output<typeof flagSchema>;
 
 export const getLaunchDarklyFlagsResponse = object({
   items: array(flagSchema),
+  totalCount: number(),
 });
 
 export const getLaunchDarklyEnvironmentsResponse = object({
   items: array(object({ key: string() })),
 });
 
+const FLAGS_PAGE_LIMIT = 100;
+
 export async function getAllFlags(projectKey: string, apiKey?: string) {
-  const response = await getClient(getApiKey(apiKey)).get(
-    `/v2/flags/${projectKey}?limit=1000`
-  );
+  const client = getClient(getApiKey(apiKey));
+  const allFlags: LaunchDarklyFlag[] = [];
+  let offset = 0;
+  let totalCount = 0;
 
-  const data = safeParse(getLaunchDarklyFlagsResponse, response.data);
+  do {
+    const response = await client.get(
+      `/v2/flags/${projectKey}?limit=${FLAGS_PAGE_LIMIT}&offset=${offset}`,
+    );
 
-  if (!data.success) {
-    throw new LaunchDarklyUnexpectedResponse();
-  }
+    const data = safeParse(getLaunchDarklyFlagsResponse, response.data);
 
-  return data.output;
+    if (!data.success) {
+      throw new LaunchDarklyUnexpectedResponse();
+    }
+
+    allFlags.push(...data.output.items);
+    totalCount = data.output.totalCount;
+    offset += FLAGS_PAGE_LIMIT;
+  } while (offset < totalCount);
+
+  return { items: allFlags };
 }
 
 export async function getAllEnvironments(projectKey: string, apiKey?: string) {
   const response = await getClient(getApiKey(apiKey)).get(
-    `/v2/projects/${projectKey}/environments?limit=100`
+    `/v2/projects/${projectKey}/environments?limit=100`,
   );
 
   const data = safeParse(getLaunchDarklyEnvironmentsResponse, response.data);
@@ -85,7 +100,10 @@ export async function getAllEnvironments(projectKey: string, apiKey?: string) {
 
 function getClient(apiKey: string) {
   const client = axios.create({
-    headers: { Authorization: apiKey },
+    headers: {
+      Authorization: apiKey,
+      'LD-API-Version': LAUNCHDARKLY_API_VERSION,
+    },
     baseURL: LAUNCHDARKLY_BASE_URL,
   });
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const GITHUB_ISSUE_URL =
   'https://github.com/jonlambert/launchdarkly-typegen/issues';
 export const LAUNCHDARKLY_BASE_URL = 'https://app.launchdarkly.com/api';
+export const LAUNCHDARKLY_API_VERSION = '20240415';
